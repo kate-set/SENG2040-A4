154bbf418f418ece45b1b2d2781821c17e38bc0e
Kate Stroganova <Kstroganova3811@conestogac.on.ca>
2024-03-31 19:44:02 -0400

    Implemented like button with ajax

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse  
 
 # Create your views here.
 
@@ -21,10 +21,25 @@ def load_post_data_view(request, num_posts):
             'title': obj.title,
             'body': obj.body,
             'liked': True if request.user in obj.liked.all() else False,
+            'count': obj.like_count,
             'author': obj.author.user.username
         }
         data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size':size})
+
+def like_unlike_post(request):
+    if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        pk = request.POST.get('pk')
+        obj = Post.objects.get(pk=pk)
+        if request.user in obj.liked.all():
+            liked = False
+            obj.liked.remove(request.user)
+        else:
+            liked = True
+            obj.liked.add(request.user)
+        return JsonResponse({'liked': liked, 'count': obj.like_count})
+    else:
+        return HttpResponse(status=400)
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
