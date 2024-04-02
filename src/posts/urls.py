e18d810eb4d23baa3488534d12735e7f400f2a68
Kate Stroganova <Kstroganova3811@conestogac.on.ca>
2024-04-02 15:29:34 -0400

    Addedupdate and delete views

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -5,6 +5,8 @@ from .views import (
     like_unlike_post,
     post_detail,
     post_detail_data_view,
+    delete_post,
+    update_post,
 )
 
 app_name = 'posts'
@@ -13,7 +15,8 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post_detail'),
-
+    path('<pk>/update/', update_post, name='post_update'),
+    path('<pk>/delete/', delete_post, name='post_delete'),
 
     path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
     path('<pk>/data/', post_detail_data_view, name='post_detail_data'),
