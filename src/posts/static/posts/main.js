cf7b631c9164793d7d1f3e2b3ffaa67fdbbc734b
Kate Stroganova <Kstroganova3811@conestogac.on.ca>
2024-04-02 12:02:13 -0400

    Implemented resetting the form after adding new posts

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -147,6 +147,7 @@ postForm.addEventListener('submit', e=>{
             likeUnlikePosts()
             $('#addPostModal').modal('hide')
             handleAlerts('success', 'New post added!')
+            postForm.reset()
         },
         error: function(error) {
             console.log(error)
