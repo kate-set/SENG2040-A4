56fa6dc9d424fbd0734477043e2ea2bd9e370013
Kate Stroganova <Kstroganova3811@conestogac.on.ca>
2024-04-02 15:05:30 -0400

    Continued working on the post detail page

--- src/posts/views.py
+++ src/posts/views.py
@@ -67,7 +67,7 @@ def post_detail_data_view(request, pk):
         'title': obj.title,
         'body': obj.body,
         'author': obj.author.user.username,
-        'logged in': request.user.username,
+        'logged_in': request.user.username,
     }
     return JsonResponse({'data': data})
 
